99c7cff23aba721bc8e6a22340a10d56021d70cb
wxttt <wangxiaottt@gmail.com>
2013-11-20 11:20:04 +0800

    add hex mode

--- public/javascripts/tv.js
+++ public/javascripts/tv.js
@@ -4,16 +4,18 @@
 (function(){
 
     var _body = document.body,
-        _cnt = document.getElementsByClassName('m-slide')[0];
+        _slide = document.getElementsByClassName('m-slide')[0],
+        _hex = document.getElementsByClassName('m-hex')[0],
+        _ctrl = document.getElementsByClassName(('m-ctrl'))[0];
 
-    _cnt.addEventListener('click', function(_event){
+    _slide.addEventListener('click', function(_event){
         var _midEl = document.getElementsByClassName('z-crt')[0],
             _leftEl = document.getElementsByClassName('z-left')[0],
             _rightEl = document.getElementsByClassName('z-right')[0];
 
         delClass(_midEl, 'z-crt');
         addClass(_midEl, 'z-small');
-        addClass(_midEl, 'z-left');
+        addClass(_midEl, 'z-left');                           ;
 
         delClass(_leftEl, 'z-left');
         addClass(_leftEl, 'z-right');
@@ -21,6 +23,22 @@
         delClass(_rightEl, 'z-small');
         delClass(_rightEl, 'z-right');
         addClass(_rightEl, 'z-crt');
+    },false)
+
+    _ctrl.addEventListener('click', function(_event){
+        var _el = _event.srcElement;
+
+        var _action = _el.getAttribute('data-action');
+        switch(_action){
+            case '1':
+                _hex.style.display = 'none'
+                _slide.style.display = 'block';
+                break;
+            case '2':
+                _slide.style.display = 'none';
+                _hex.style.display = 'block';
+                break;
+        }
     },false)
 
 
