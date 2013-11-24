66a1d135b1c4d211749de93f9070da53f5031c8a
wxttt <wangxiaottt@gmail.com>
2013-11-24 21:46:29 +0800

    add simple css3 animation

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,14 +1,3 @@
-//给猪头的全局方法
-window.turnLeft = function(){
-    alert('turn left');
-}
-
-window.turnRight = function(){
-    alert('turn right');
-}
-
-
-
 function tvCtrl($scope, $http){
     $scope.name = '分类';
     $scope.colors = ['green', 'black', 'brown']
@@ -31,4 +20,21 @@ function tvCtrl($scope, $http){
         console.log($scope.data_2);
     })
 
+}
+
+window.onload = function(){
+    var list1 = document.getElementsByClassName('m-list-1')[0],
+        list2 = document.getElementsByClassName('m-list-2')[0];
+
+
+    //给猪头的全局方法
+    window.turnLeft = function(){
+        list1.style["-webkit-transform"] = 'translate3d(-500px, 0, 0)';
+        list2.style["-webkit-transform"] = 'translate3d(-500px, 0, 0)';
+    }
+
+    window.turnRight = function(){
+        list1.style["-webkit-transform"] = 'translate3d(0, 0, 0)';
+        list2.style["-webkit-transform"] = 'translate3d(0, 0, 0)';
+    }
 }
